67fb8ea34f8b86da91b02129762e0f6010045c54
Sebastian Varga <sebvarga@gmail.com>
2020-09-22 13:42:41 +0200

    new stuff

--- randomfile.py
+++ randomfile.py
@@ -6,38 +6,43 @@
 
 import os, sys, getopt
 
-def main(argv):
-   filesize = 1024
-   fileamount = 1
-   fileprefix = 'outputfile'
-   try:
-       opts, args = getopt.getopt(argv,"hs:a:p:",["fsize=","famount=","prefix="])
-   except getopt.GetoptError:
-       print("usage: randomfile.py -s <filesize_in_Byte> -a <amount_of_files> -p <fileprefix>")
-       sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('usage: randomfile.py -s <filesize_in_Byte> -a <amount_of_files> -p <fileprefix>')
-         sys.exit()
-      elif opt in ("-s", "--fsize"):
-         filesize = arg
-      elif opt in ("-a", "--afiles"):
-         fileamount = arg
-      elif opt in ("-p", "--prefix"):
-         fileprefix = arg
+filesize = 153600
+fileamount = 24
+fileprefix = 'outputfile_'
+
+# def main(argv):
+#    filesize = 1024
+#    fileamount = 1
+#    fileprefix = 'outputfile'
+#    try:
+#        opts, args = getopt.getopt(argv,"hs:a:p:",["fsize=","famount=","prefix="])
+#    except getopt.GetoptError:
+#        print("usage: randomfile.py -s <filesize_in_Byte> -a <amount_of_files> -p <fileprefix>")
+#        sys.exit(2)
+#    for opt, arg in opts:
+#       if opt == '-h':
+#          print('usage: randomfile.py -s <filesize_in_Byte> -a <amount_of_files> -p <fileprefix>')
+#          sys.exit()
+#       elif opt in ("-s", "--fsize"):
+#          filesize = opt
+#       elif opt in ("-a", "--afiles"):
+#          fileamount = opt
+#       elif arg in ("-p", "--prefix"):
+#          fileprefix = arg
 
 
-if __name__ == "__main__":
-   main(sys.argv[1:])
+# if __name__ == "__main__":
+#    main(sys.argv[1:])
 
-   filesize = argv[1]
-   fileamount = argv[2]
-   fileprefix = argv[3]
-   print ('Filesize is', filesize)
-   print ('amount of generated files:', fileamount)
-   print ('fileprefix is:', fileprefix)
+   # filesize = argv[1]
+   # fileamount = argv[2]
+   # fileprefix = argv[3]
+print ('Filesize is', filesize)
+print ('amount of generated files:', fileamount)
+print ('fileprefix is:', fileprefix)
 
-   for i in range (0, fileamount):
-       suffix = str(i+1)
-       with open('fileprefix+suffix', 'wb') as fout:
-           fout.write(os.urandom(filesize))
+for i in range (0, fileamount):
+    filename = fileprefix+str(i+1)
+    print(filename)
+    with open(filename, 'wb') as fout:
+        fout.write(os.urandom(filesize))
